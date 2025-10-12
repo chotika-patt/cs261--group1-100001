5f58a8292e7365e7d0abf27effe0db70292b57ce
Rickoroxd <0954744487zaz@gmail.com>
2025-10-12 19:24:23 +0700

    Backend - Add SercurityConfig but not Secure

--- src/main/java/tu_store/demo/security/SecurityConfig.java
+++ src/main/java/tu_store/demo/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 public class SecurityConfig {
     @Bean(name = "mySecurityConfig")
